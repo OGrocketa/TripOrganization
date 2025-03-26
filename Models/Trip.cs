3a468fca3eb2e0565d66a8ab28353b8282ce9bbf
OGrocketa <jaroslaw.sedach@gmail.com>
2025-03-26 14:15:26 +0100

    wip

---
+++ Models/Trip.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+namespace TripOrganization.Models;
+
+public class Trip{
+    public int Id { get; set; }
+    public string? Title {get;set;}
+    public int Capacity {get; set;}
+    public string? Data {get; set;}
+
+    [Range(0, double.MaxValue, ErrorMessage = "Cost must be a positive number")]
+    [DataType(DataType.Currency)]
+    public decimal Cost { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime Date { get; set; }
+
+}
